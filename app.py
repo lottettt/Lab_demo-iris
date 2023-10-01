4932fb5f308b4c9d3f79f2e5f2983bd72a0dba0c
lottettt <137585702+lottettt@users.noreply.github.com>
2023-10-01 15:44:28 +0700

    new version

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 import pickle
 import warnings
 import streamlit as st
+from PIL import Image
 
 warnings.filterwarnings("ignore")
 
-from PIL import Image
-
-pickle_in = open("best_rf_model.pkl", "rb")
+pickle_in = open(r"model_iris.pkl", "rb")
 classifier = pickle.load(pickle_in)
 
 def predict_iris_variety(sepal_length, sepal_width, petal_length, petal_width):
     prediction = classifier.predict([[sepal_length, sepal_width, petal_length, petal_width]])
-    print(prediction)
     return prediction
 
 def input_output():
@@ -19,17 +17,24 @@ def input_output():
     st.image("https://machinelearninghd.com/wp-content/uploads/2021/03/iris-dataset.png", width=600)
     st.markdown("You are using Streamlit...", unsafe_allow_html=True)
 
-    sepal_length = float(st.text_input("Enter Sepal Length", "."))
-    sepal_width = float(st.text_input("Enter Sepal Width", "."))
-    petal_length = float(st.text_input("Enter Petal Length", "."))
-    petal_width = float(st.text_input("Enter Petal Width", "."))
-
+    sepal_length = st.text_input("Enter Sepal Length", "")
+    sepal_width = st.text_input("Enter Sepal Width", "")
+    petal_length = st.text_input("Enter Petal Length", "")
+    petal_width = st.text_input("Enter Petal Width", "")
 
     result = ""
     if st.button("Click here to Predict"):
-        result = predict_iris_variety(sepal_length, sepal_width, petal_length, petal_width)
-        st.balloons()
-        st.success(f'The output is {result}')
+        try:
+            sepal_length = float(sepal_length)
+            sepal_width = float(sepal_width)
+            petal_length = float(petal_length)
+            petal_width = float(petal_width)
+
+            result = predict_iris_variety(sepal_length, sepal_width, petal_length, petal_width)
+            st.balloons()
+            st.success(f'The output is {result}')
+        except ValueError:
+            st.error("Please enter valid numerical values for all input fields.")
 
 if __name__ == '__main__':
     input_output()
